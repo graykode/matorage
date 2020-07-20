0a01ed4f218dcd99f8aed715aeaa0e67726c4213
graykode <nlkey2022@gmail.com>
2020-07-20 17:51:09 +0900

    (edit) data saver doc string

--- matorage/data/saver.py
+++ matorage/data/saver.py
@@ -31,45 +31,50 @@ from matorage.data.uploader import DataUploader
 
 class DataSaver(object):
     r""" Dataset saver classes.
-        Although it plays a similar role as `[torch.utils.data.DataLoader](https://pytorch.org/docs/stable/data.html)`
-        , it is a class that is contradictory. If you attempt to write 100 data sequentially from 60000 data,
-        it will work like a `consumer-producer pattern` with the logic in the following order.
-            1. We know that 60000/100=600 data will be produced because 60000 data is divided into 100.
-            2. The data that builds up in production is appended to a file of unique names.
-            3. If the file size exceeds 'MIN_MIN_OBJECT_SIZE', it is uploaded to MinIO with deleted in local and the new file is written.
-        Step3 occurs asynchronously. Therefore, while step3 is in progress, step1 and 2 are in progress.
+        This class is initialized independently of the process and goes through the process of uploading
+        the numby data created through the pre-processing process to the MinIO.
+        Update the file, push the upload queue if it exceeds a certain size, close the file, and create a new file.
+        After saving, you should disconnect the data saver.
 
-        To make the above procedure easier to understand, the following is written in the pseudo-code.
+        To make This procedure easier to understand, the following is written in the pseudo-code.
             ```python
             file is opened, if file already exist, there will be append mode.
             for data(shape : 100 x 784) in multiprocessing(dataset(shape : 60000 x 784))
-                if MIN_MIN_OBJECT_SIZE <= file size
+                if MIN_OBJECT_SIZE <= file size <= MAX_OBJECT_SIZE
                     file is closed
                     lock other processes until new_file is opened
                     new_file is opened
                     new_file.append(data)
-                    asynchronously upload to backend storage
+                    upload to backend storage
                 else
                     file.append(data)
             file is closed
             ```
-        In order to prevent multiple processes entering into the 'MIN_OBJECT_SIZE E= file size' during multiprocessing,
-        other processes must be locked for a while until a new file is created.
 
         Note:
-            - Both Tensorflow and Pytorch are eligible for this class.
-            - According to the [MinIO python client document](https://docs.min.io/docs/python-client-api-reference.html),
-            MinIO is a thread safety, but during multiprocessing, you must create a new MinIO Class object for each process.
-
-            Python supports asynchronous function with asyncio, however it's not in minio function.
-            So we use multi-thread to avoid blocking-io with thread job queue.
+            - Deep Learning Framework Type : All(pure python is also possible)
+            - **All processes should call the constructors of this class independently.**
 
         Args:
             config (:obj:`matorage.config.MTRConfig`, `require`):
-                S3 object storage endpoint.
 
         Example::
+            Single Process example
+                ```python
+                data_saver = DataSaver(config=data_config)
+                row = 100
+                data = np.random.rand(64, 3, 224, 224)
 
+                start = time.time()
+
+                for _ in tqdm(range(row)):
+                    preprocessing_work()
+                    data_saver({
+                        'array' : data
+                    })
+
+                data_saver.disconnect()
+                ```
     """
 
     def __init__(self, config):
