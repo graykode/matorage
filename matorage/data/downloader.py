0eaea95de414d6f2176d59530295de3af69c7ff0
graykode <nlkey2022@gmail.com>
2020-07-28 16:35:37 +0900

    (init) torch dataset and download

--- matorage/data/downloader.py
+++ matorage/data/downloader.py
@@ -11,3 +11,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import os
+from minio import ResponseError
+
+from matorage.connector import MRTConnector
+
+class DataDownloader(MRTConnector):
+    r""" File Storage downloader class with multi thread.
+        MinIO is thread-safety, according to document.
+        Although Python Global Interpreter Lock(GIL), multi thread can benefit greatly from file IO.
+    """
+
+    def __init__(self, client, bucket, num_worker_threads):
+        super(DataDownloader, self).__init__(client, bucket, num_worker_threads)
+
+    def do_job(self, local_file, remote_file):
+        if isinstance(remote_file, str):
+            minio_key = os.path.basename(remote_file)
+        try:
+            self._client.fget_object(
+                self._bucket, minio_key, local_file
+            )
+        except ResponseError as err:
+            print(err)
--- matorage/data/torch/dataset.py
+++ matorage/data/torch/dataset.py
@@ -16,110 +16,134 @@ import os
 import json
 import time
 import torch
+import atexit
 import tables
 import bisect
 import tempfile
 from minio import Minio
-from multiprocessing import Manager
+from os.path import expanduser
+from multiprocessing import Manager, Barrier
 
 from torch.utils.data import Dataset
 
 from matorage.nas import NAS
 from matorage.utils import check_nas
+from matorage.data.downloader import DataDownloader
+from matorage.utils import logger
 
 class MTRDataset(Dataset):
     r"""MTRDataset class for Pytorch Dataset
 
-        Note:
-
+        This class is customized for the dataset of the PyTorch, so it is operated by the following procedure.
+        1. The `_object_file_mapper` manages the minio object as key and the downloaded local path as value.
+            {'tmpv7sy5_1fff7845eccd874068.h5': '/tmp/tmpja6wo221tmpv7sy5_1fff7845eccd874068.h5'}
+            When minio object is downloaded, it is recorded in _object_file_maper.
+        2. We read `_object_file_mapper` and download only new objects that are not there.
+        3. `__getitem__` brings numpy data in local data from data index.
 
         Args:
             config (:obj:`matorage.config.MTRConfig`, `require`):
-            download (:obj:`boolean`, `optional`, defaults to `True`):
-                download h5 file in local temp folder.
-                if using `batch_atomic` or `inmemory` options, then set it `False`
-                Setting this option allows you to occupy a lot of local storage volumes, but speeds up
-                by not downloadingthem again twice for the next epoch or unused index.
+            num_worker_threads :obj:`int`, `optional`, defaults to `4`):
+                    number of backend storage worker to upload or download.
             clear (:obj:`boolean`, `optional`, defaults to `True`):
                 Delete all files stored on the local storage after the program finishes.
 
+        HDF5 Options
+            inmemory (:obj:`bool`, `optional`, defaults to `False`):
+                If you use this value as `True`, then you can use `HDF5_CORE` driver (https://support.hdfgroup.org/HDF5/doc/TechNotes/VFL.html#TOC1)
+                so the temporary file for uploading or downloading to backend storage,
+                such as MinIO, is not stored on disk but is in the memory.
+                Keep in mind that using memory is fast because it doesn't use disk IO, but it's not always good.
+                If default option(False), then `HDF5_SEC2` driver will be used on posix OS(or `HDF5_WINDOWS` in Windows).
+
     """
-    def __init__(self, config, inmemory=False, clear=True):
+    def _init_download(self):
+        _client = self._create_client()
+        _downloader = DataDownloader(
+            client=_client,
+            bucket=self.config.bucket_name,
+            num_worker_threads=self.num_worker_threads
+        )
+
+        _remote_files = list(self.reindexer.values())
+        for _remote_file in _remote_files:
+            _local_file = tempfile.mktemp(_remote_file)
+            if _remote_file not in self._object_file_mapper:
+                self._object_file_mapper[_remote_file] = _local_file
+                _downloader.set_queue(local_file=_local_file, remote_file=_remote_file)
+        _downloader.join_queue()
+
+        with open(self.cache_path, "w") as f:
+            json.dump(self._object_file_mapper, f)
+
+    def _pre_open_files(self):
+        self.open_files = {}
+        for _remote, _local in self._object_file_mapper.items():
+            self.open_files[_remote] = tables.open_file(_local, 'r')
+
+    def __init__(self, config, num_worker_threads=4, clear=True, inmemory=False, cache_folder_path='~/.matorage'):
         self.config = config
+        self.attribute = self._set_attribute()
+
+        # Storage configuration
+        self.num_worker_threads = num_worker_threads
+
         self.download = False if (config.batch_atomic or inmemory) else True
         self.clear = False if not self.download else clear
+
+        self.cache_folder_path = expanduser(cache_folder_path)
+        if not os.path.exists(self.cache_folder_path):
+            os.makedirs(self.cache_folder_path)
+
+        self.cache_path = f"{os.path.join(self.cache_folder_path, self.config.bucket_name)}.json"
+        if os.path.exists(self.cache_path):
+            with open(self.cache_path) as f:
+                self._object_file_mapper = json.load(f)
+        else:
+            self._object_file_mapper = {}
 
         self.reindexer = self._merge_metadata(
             bucket_name=self.config.bucket_name
         )
-        self.ends = list(self.reindexer.keys())
-
-        self._clients = {}
-
-        # To avoid processes race condition.
-        _manager = Manager()
-        self._worker_job = dict()
-        self._object_file_mapper = _manager.dict()
-
-        self.num_worker = -1
-
-    def __len__(self):
-        return self.ends[-1]
-
-    def __getitem__(self, idx):
-        if self.num_worker == -1:
-            self._set_num_worker()
-            self._balance_worker()
-            self._create_clients()
-
-        _worker_info = torch.utils.data.get_worker_info()
-        if _worker_info is None:
-            worker_id = 0
-        else:
-            worker_id = _worker_info.id
-
-        _objectname, _index = self._find_object(idx)
-
-        # memoization : if _filename is in local file or in-memory return it.
-        if _objectname in self._object_file_mapper:
-            self._get_from_local_file_or_memory()
-            return 0
-
-        print(worker_id, _objectname, self._object_file_mapper)
-        while _objectname not in self._object_file_mapper and \
-                self._worker_job[_objectname] != worker_id:
-            time.sleep(0.00001)
+        self.end_indices = list(self.reindexer.keys())
 
         if self.download:
-            if _objectname not in self._object_file_mapper:
-                _local_filename = tempfile.mktemp(_objectname)
-                self._clients[worker_id].fget_object(
-                    bucket_name=self.config.bucket_name,
-                    object_name=_objectname,
-                    file_path=_local_filename
+            self._init_download()
+            logger.info('All Dataset Downloaded Done.')
+        self._pre_open_files()
+
+        assert len(self._object_file_mapper) == len(self.reindexer)
+        atexit.register(self._exit)
+
+    def _exit(self):
+        for _file in list(self.open_files.values()):
+            _file.close()
+        if self.clear:
+            for _local_file in list(self._object_file_mapper.values()):
+                os.remove(_local_file)
+            if os.path.exists(self.cache_path):
+                os.remove(self.cache_path)
+
+    def __len__(self):
+        return self.end_indices[-1]
+
+    def __getitem__(self, idx):
+        _objectname, _relative_index = self._find_object(idx)
+        if _objectname in self._object_file_mapper:
+            _file = self.open_files[_objectname]
+
+            _attr_names = list(_file.get_node("/")._v_children.keys())
+            return_tensor = {}
+            for _attr_name in _attr_names:
+                return_tensor[_attr_name] = self._reshape_convert_tensor(
+                    numpy_array=_file.root[_attr_name][_relative_index],
+                    attr_name=_attr_name
                 )
-                self._object_file_mapper[_objectname] = _local_filename
-                print(worker_id, _objectname, 'done')
-
-            # to read file, blocking until download finish.
-
-            return 0
+            return list(return_tensor.values())
         else:
-            # if not set `batch_atomic` first find in lru cache.
-            _object = self._clients[worker_id].get_object(
-                bucket_name=self.config.bucket_name,
-                object_name=_filename
-            )
-
-            return 0
-
-    def _balance_worker(self):
-        _files = list(self.reindexer.values())
-        _idx = 0
-        for _file in _files:
-            if _idx == self.num_worker:
-                _idx = 0
-            self._worker_job[_file] = _idx
+            raise ValueError("objectname({}) is not exist in {}".format(
+                _objectname, self._object_file_mapper
+            ))
 
     def _create_client(self):
         return Minio(
@@ -129,11 +153,6 @@ class MTRDataset(Dataset):
             secure=self.config.secure,
         ) if not check_nas(self.config.endpoint) else NAS(self.config.endpoint)
 
-    def _create_clients(self):
-        # create minio client once in a process to avoid thread unsafe
-        for w in range(self.num_worker):
-             self._clients[w] = self._create_client()
-
     def _find_object(self, index):
         """
         find filename by index with binary search algorithm(indexes had been sorted).
@@ -141,14 +160,11 @@ class MTRDataset(Dataset):
         Returns:
             :obj:`str`: filename for index
         """
-        _key_idx = bisect.bisect_right(self.ends, index)
-        _key = self.ends[_key_idx]
-        _last_key = self.ends[_key_idx - 1] if _key_idx else 0
+        _key_idx = bisect.bisect_right(self.end_indices, index)
+        _key = self.end_indices[_key_idx]
+        _last_key = self.end_indices[_key_idx - 1] if _key_idx else 0
         _relative_index = (index - _last_key)
         return self.reindexer[_key], _relative_index
-
-    def _get_from_local_file_or_memory(self):
-        pass
 
     def _merge_metadata(self, bucket_name):
         """
@@ -185,9 +201,28 @@ class MTRDataset(Dataset):
 
         return reindexer
 
-    def _set_num_worker(self):
-        _worker_info = torch.utils.data.get_worker_info()
-        if _worker_info is None:
-            self.num_worker = 0
-        else:
-            self.num_worker = _worker_info.num_workers
+    def _set_attribute(self):
+        """
+        Set `attribute` dictionary.
+
+        Returns:
+            :obj:`dict` : attribute
+            {
+                'image': {'shape': (28, 28), 'type': 'uint8'},
+                'target': {'shape': (1,), 'type': 'uint8'}
+            }
+        """
+        _attributes = {}
+        _metadata_attributes = self.config.metadata.attributes
+        for _attr in _metadata_attributes:
+            _attributes[_attr.name] = {
+                "shape" : tuple([-1] + list(_attr.shape)),
+                "type" : str(_attr.type.type)
+            }
+        return _attributes
+
+    def _reshape_convert_tensor(self, numpy_array, attr_name):
+        _shape = self.attribute[attr_name]["shape"]
+        # numpy_array = numpy_array.reshape(_shape)
+        # tensor = torch.from_numpy(numpy_array)
+        return numpy_array
