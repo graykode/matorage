c5d80c6a68c50735e5cdf947d169d4ef1b82be4e
graykode <nlkey2022@gmail.com>
2020-07-30 02:37:02 +0900

    (refactor) Tensorflow v2 dataloader train, test are False default

--- examples/tensorflow/mnist_dataloader.py
+++ examples/tensorflow/mnist_dataloader.py
@@ -68,8 +68,8 @@ def test_step(images, labels):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Tensorflow V2 MNIST Example')
-    parser.add_argument('--train', action='store_true', default=True)
-    parser.add_argument('--test', action='store_true', default=True)
+    parser.add_argument('--train', action='store_true', default=False)
+    parser.add_argument('--test', action='store_true', default=False)
     args = parser.parse_args()
 
     if args.train:
