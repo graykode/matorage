6459ecbe493b871c2e767fdd724fa917765ccdfe
graykode <nlkey2022@gmail.com>
2020-07-29 20:15:17 +0900

    (refactor) tensorflow v2 dataloader

--- examples/tensorflow/mnist_dataloader.py
+++ examples/tensorflow/mnist_dataloader.py
@@ -12,45 +12,115 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# https://www.tensorflow.org/tutorials/quickstart/advanced
+
 import time
 import argparse
 from tqdm import tqdm
 import tensorflow as tf
-from tensorflow.keras import datasets
+from tensorflow.keras import datasets, layers, optimizers, Sequential, metrics
 
 from matorage import *
 from matorage.tensorflow import *
 
+model = Sequential([
+    layers.Reshape((28 * 28,)),
+    layers.Dense(512, activation='relu'),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(10)
+])
+model.build(input_shape=(None, 28 * 28))
+optimizer = optimizers.Adam(lr=0.01)
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+@tf.function
+def train_step(image, target):
+    with tf.GradientTape() as tape:
+        output = model(image)
+        loss = tf.nn.softmax_cross_entropy_with_logits(
+            logits=output,
+            labels=tf.one_hot(target, depth=10)
+        )
+        loss = tf.reduce_sum(loss) / 64
+
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(target, output)
+
+@tf.function
+def test_step(images, labels):
+    output = model(images)
+    loss = tf.nn.softmax_cross_entropy_with_logits(
+        logits=output,
+        labels=tf.one_hot(target, depth=10)
+    )
+
+    test_loss(loss)
+    test_accuracy(labels, output)
+
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser = argparse.ArgumentParser(description='Tensorflow V2 MNIST Example')
     parser.add_argument('--train', action='store_true', default=True)
     parser.add_argument('--test', action='store_true', default=True)
     args = parser.parse_args()
 
-    traindata_config = DataConfig(
-        endpoint='127.0.0.1:9000',
-        access_key='minio',
-        secret_key='miniosecretkey',
-        dataset_name='mnist',
-        additional={
-            "mode": "train"
-        },
-    )
-    train_dataset = MTRDataset(config=traindata_config, clear=True)
+    if args.train:
+        traindata_config = DataConfig(
+            endpoint='127.0.0.1:9000',
+            access_key='minio',
+            secret_key='miniosecretkey',
+            dataset_name='mnist',
+            additional={
+                "mode": "train",
+                "framework": "tensorflow"
+            },
+        )
+        train_dataset = MTRDataset(config=traindata_config, shuffle=True, batch_size=64, clear=True)
+
+    if args.test:
+        testdata_config = DataConfig(
+            endpoint='127.0.0.1:9000',
+            access_key='minio',
+            secret_key='miniosecretkey',
+            dataset_name='mnist',
+            additional={
+                "mode": "test",
+                "framework": "tensorflow"
+            },
+        )
+        test_dataset = MTRDataset(config=testdata_config, batch_size=64, clear=True)
+
+    epochs = 5
+    template = 'Epoch: {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
 
     start = time.time()
 
-    train_loader = tf.data.Dataset.from_tensor_slices(train_dataset.get_objectnames)
-    train_loader = train_loader.interleave(lambda filename: tf.data.Dataset.from_generator(
-        train_dataset,
-        (tf.float32, tf.int64),
-        (tf.TensorShape([28, 28]), tf.TensorShape([1])),
-        args=(filename,)), cycle_length=4).batch(64)
-
-    for batch_idx, (image, target) in enumerate(tqdm(train_loader, total=60000//64)):
-        print(image.shape, target.shape)
-        pass
+    for e in range(5):
+        if args.train:
+            for batch_idx, (image, target) in enumerate(
+                tqdm(train_dataset.dataloader, total=60000 // 64)
+            ):
+                train_step(image, target)
+        if args.test:
+            for batch_idx, (image, target) in enumerate(
+                tqdm(test_dataset.dataloader, total=10000//64)
+            ):
+                test_step(image, target)
+        print(
+            template.format(e + 1,
+            train_loss.result(),
+            train_accuracy.result() * 100,
+            test_loss.result(),
+            test_accuracy.result() * 100)
+        )
 
     end = time.time()
     print(end - start)
