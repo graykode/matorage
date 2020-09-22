5e00fe0819247e480e2b9d067a170a863c15cb5e
graykode <nlkey2022@gmail.com>
2020-09-22 19:28:46 +0900

    (fixed) change cache folder path, (add) setUp function

--- tests/test_data.py
+++ tests/test_data.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import os
+import shutil
 import unittest
 from minio import Minio
 from urllib.parse import urlsplit
@@ -34,6 +35,7 @@ class DataTest(unittest.TestCase):
     nas_config = {
         "endpoint": "/tmp/unittest",
     }
+    cache_folder_path = "/tmp/unittest_cache"
 
     def check_nas(self, endpoint):
         _url_or_path = "//" + endpoint
@@ -43,6 +45,10 @@ class DataTest(unittest.TestCase):
         if u.netloc:
             return False
         raise ValueError("This endpoint is not suitable.")
+
+    def setUp(self):
+        if os.path.exists(self.cache_folder_path):
+            shutil.rmtree(self.cache_folder_path)
 
     def tearDown(self):
         if self.data_saver is not None:
@@ -63,8 +69,8 @@ class DataTest(unittest.TestCase):
                     os.remove(_file)
 
         if self.dataset is not None and not self.check_nas(self.dataset.config.endpoint):
-            if os.path.exists(self.dataset.cache_path):
-                os.remove(self.dataset.cache_path)
+            if os.path.exists(self.cache_folder_path):
+                shutil.rmtree(self.cache_folder_path)
 
         if self.data_config_file is not None:
             if os.path.exists(self.data_config_file):
