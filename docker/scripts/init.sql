b8a8eb0448cb0f81913ca146a87d39630aee031f
graykode <nlkey2022@gmail.com>
2020-10-04 00:12:46 +0900

    (refactor) refactor database attribute names in init.sql

--- docker/scripts/init.sql
+++ docker/scripts/init.sql
@@ -1,13 +1,13 @@
 -- SET character_set_client = utf8mb4 ;
 --DROP TABLE IF EXISTS compressor CASCADE;
 --DROP TABLE IF EXISTS bucket CASCADE;
---DROP TABLE IF EXISTS item_attribute CASCADE;
+--DROP TABLE IF EXISTS attributes CASCADE;
 --DROP TABLE IF EXISTS indexer CASCADE;
 
 CREATE TABLE compressor (
   id integer primary key,
-  compressor_level integer not null,
-  compressor_lib varchar(255) not null
+  complevel integer not null,
+  complib varchar(255) not null
 );
 
 CREATE TABLE bucket (
@@ -20,20 +20,21 @@ CREATE TABLE bucket (
   constraint compressor_id foreign key(compressor_id) references compressor(id)
 );
 
-CREATE TABLE item_attribute (
+CREATE TABLE attributes (
   id integer primary key not null,
-  item_size integer not null,
-  attribute_shape varchar(255),
-  attribute_type varchar(255),
-  bucket_id varchar(255), 
+  name varchar(255),
+  type varchar(255),
+  shape varchar(255),
+  itemsize integer not null,
+  bucket_id varchar(255),
   constraint bucket_id foreign key (bucket_id) references bucket(id)
 );
 
 CREATE TABLE indexer (
   id integer primary key not null,
-  indexer_start bigint not null,
-  indexer_length integer not null,
-  indexer_name varchar(255) not null,
+  start bigint not null,
+  length integer not null,
+  name varchar(255) not null,
   bucket_id varchar(255),
   constraint bucket_id foreign key (bucket_id) references bucket(id)
 );
